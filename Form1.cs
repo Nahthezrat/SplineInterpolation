7684c3cce9cd3a50a2f63bdb3043b544279c6c75
NahtHezrat <nahthezrat@gmail.com>
2019-11-14 08:43:14 -0600

    SM(vA1)

--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Windows;
 
 namespace SplineInterpolation
 {
@@ -22,33 +23,74 @@ namespace SplineInterpolation
 
         private void SplineInterpolation(double [,] Nodes)
         {
-            int dim = Nodes.GetLength(0);
-            var a_matrix = new double[dim, dim];
-            var x_vector = new double[dim];
-            var b_vector = new double[dim];
+            int N = Nodes.GetLength(0);
+            var b_vector = new double[N];
+            var c_vector = new double[N];
+            var d_vector = new double[N];
 
-            for (int i = 0; i < dim; ++i)
+            var x_vector = new double[N];
+            var delta_x = new double[N];
+
+            var alpha_vector = new double[N];
+            var beta_vector = new double[N];
+            var gamma_vector = new double[N];
+
+            var a_matrix = new double[N, N]; // Тридиагональная матрица
+            var coefficient_matrix = new double[4][];
+
+            for (int i = 0; i < 4; i++)
             {
-                b_vector[i] = 3*(((Nodes[i + 1, 1] - Nodes[i, 1])/(Nodes[i + 1, 0] - Nodes[i, 0])) - ((Nodes[i, 1] - Nodes[i - 1, 1]) / (Nodes[i, 0] - Nodes[i - 1, 0])));
+                coefficient_matrix[i] = new double[N - 1];
             }
 
-            double a, b, c, d;
-            for(int i = 0; i < dim; ++i)
+            for (int i = 0; i + 1 <= N - 1; i++)
             {
-                // h[k] = x[k] - x[k-1]
-                /*a = Nodes[i, 0] - Nodes[i - 1, 0];
-                b = Nodes[i + 1, 0] - Nodes[i, 0];
-                c = 2 * ((Nodes[i, 0] - Nodes[i - 1, 0]) + (Nodes[i + 1, 0] - Nodes[i, 0]));*/
+                delta_x[i] = Nodes[i + 1, 0] - Nodes[i, 0];
+                if (delta_x[i] == 0.0)
+                {
+                    //return null;
+                }
+            }
 
-                a_matrix[i, i] = (i==0 || i==dim-1) ? 0 : (2 * ((Nodes[i, 0] - Nodes[i - 1, 0]) + (Nodes[i + 1, 0] - Nodes[i, 0]))); // C
-                if (i >= 0 && i + 1 < dim) { a_matrix[i + 1, i] = Nodes[i + 1, 0] - Nodes[i, 0]; } // A
-                if (i + 1 < dim) { a_matrix[i, i + 1] = Nodes[i + 1, 0] - Nodes[i, 0]; } // B
+            // b[i] = 3 * ( (f[i] - f[i - 1])*delta_x[i]/delta_x[i - 1] + (f[i + 1] - f[i])*delta_x[i - 1]/delta_x[i] ),  i = 1, ... , N - 2
+            for (long i = 1; i + 1 <= N - 1; i++)
+            {
+                b_vector[i] = 3.0 * (delta_x[i] * ((Nodes[i, 1] - Nodes[i - 1, 1]) / delta_x[i - 1]) + delta_x[i - 1] * ((Nodes[i + 1, 1] - Nodes[i, 1]) / delta_x[i]));
+            }
+            b_vector[0] = ((delta_x[0] + 2.0 * (Nodes[2, 0] - Nodes[0, 0])) * delta_x[1] * ((Nodes[1, 1] - Nodes[0, 1]) / delta_x[0]) + Math.Pow(delta_x[0], 2.0) * ((Nodes[2, 1] - Nodes[1, 1]) / delta_x[1])) / (Nodes[2, 0] - Nodes[0, 0]);
+            b_vector[N - 1] = (Math.Pow(delta_x[N - 1 - 1], 2.0) * ((Nodes[N - 2, 1] - Nodes[N - 3, 1]) / delta_x[N - 1 - 2]) + (2.0 * (Nodes[N - 1, 0] - Nodes[N - 3, 0]) + delta_x[N - 1 - 1]) * delta_x[N - 1 - 2] * ((Nodes[N - 1, 1] - Nodes[N - 2, 1]) / delta_x[N - 1 - 1])) / (Nodes[N - 1, 0] - Nodes[N - 3, 0]);
+
+            // Коэффициенты матрицы
+            beta_vector[0] = delta_x[1];
+            gamma_vector[0] = Nodes[2, 0] - Nodes[0, 0];
+
+            beta_vector[N - 1] = delta_x[N - 1 - 1];
+            alpha_vector[N - 1] = (Nodes[N - 1, 0] - Nodes[N - 3, 0]);
+
+            for (int i = 1; i < N - 1; i++)
+            {
+                beta_vector[i] = 2.0 * (delta_x[i] + delta_x[i - 1]);
+                gamma_vector[i] = delta_x[i];
+                alpha_vector[i] = delta_x[i - 1];
+            }
+
+            // Формирование матрицы
+            for (int i = 0; i < N; ++i)
+            {
+                a_matrix[i, i] = beta_vector[i];
+                if (i >= 0 && i < N-1) { a_matrix[i + 1, i] = alpha_vector[i + 1]; }
+                if (i < N - 1) { a_matrix[i, i + 1] = gamma_vector[i]; }
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(alpha_vector[i] + "  " + beta_vector[i] + " " + gamma_vector[i] + " | " + b_vector[i]);
             }
 
             Console.WriteLine("Linear System: ");
-            for (int i = 0; i < dim; ++i)
+            for (int i = 0; i < N; ++i)
             {
-                for (int j = 0; j < dim; ++j)
+                for (int j = 0; j < N; ++j)
                 {
                     Console.Write("{0:f3}\t", a_matrix[i, j]);
                 }
@@ -56,14 +98,45 @@ namespace SplineInterpolation
                 Console.WriteLine();
             }
 
+            // Решение методом Гаусса
+            var xf_vector = new double[N];
             var system = new LinearSystem(a_matrix, b_vector);
-
-            Console.WriteLine("X Vector: ");
-            for (int i = 0; i < dim; ++i)
+            xf_vector = system.XVector;
+            Console.WriteLine("x_vector: ");
+            for (int k = 0; k < N; ++k)
             {
-                Console.Write("{0:f3}\t", system.XVector[i]);
+                Console.WriteLine(xf_vector[k]);
             }
-            Console.WriteLine();
+
+            // Нахождение других коэффициентов
+            for (int i = 0; i < N; ++i)
+            {
+                d_vector[i] = (b_vector[i + 1] + b_vector[i] - 2 * delta[i]) / (h[i] * h[i]);
+                c_vector[i] = 2 * (delta[i] - b_vector[i]) / h[i] - (b_vector[i + 1] - delta[j]) / h[i];
+            }
+            /*
+            double c = 0.0;
+            for (long i = 0; i < N - 1; i++)
+            {
+                c = beta_vector[i];
+                b[i] /= c;
+                beta_vector[i] /= c;
+                gamma_vector[i] /= c;
+
+                c = alpha_vector[i + 1];
+                b[i + 1] -= c * b[i];
+                alpha_vector[i + 1] -= c * beta_vector[i];
+                beta_vector[i + 1] -= c * gamma_vector[i];
+            }
+
+            b[N - 1] /= beta_vector[N - 1];
+            beta_vector[N - 1] = 1.0;
+            for (long i = N - 2; i >= 0; i--)
+            {
+                c = gamma_vector[i];
+                b[i] -= c * b[i + 1];
+                gamma_vector[i] -= c * beta_vector[i];
+            }*/
         }
 
         private void Button1_Click(object sender, EventArgs e)
@@ -82,6 +155,7 @@ namespace SplineInterpolation
                 {
                     line = sstream.ReadLine();
                     string[] tokens = line.Split();
+
 
                     Nodes[i, 0] = double.Parse(tokens[0]);
                     Nodes[i, 1] = double.Parse(tokens[1]);
